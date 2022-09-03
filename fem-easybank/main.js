a3e025e68dfac3cc20f7224e72a21b0630e6d70d
demi.coder <104001201+OgaDavid@users.noreply.github.com>
2022-09-03 12:08:59 -0700

    Tablet version

--- fem-easybank/main.js
+++ fem-easybank/main.js
@@ -3,3 +3,13 @@ window.addEventListener("scroll", () => {
     navbar.classList.toggle("box-shadow", window.scrollY > 0);
     console.log("scroll");
 })
+
+const homeText = document.querySelector(".home-left h1");
+window.addEventListener("scroll", () => {
+    homeText.classList.toggle("animate__slideOutLeft", window.scrollY > 0)
+})
+
+if (window.matchMedia("(max-width: 1190px)").matches) {
+    const contactImage = document.querySelector(".contact-right img");
+    contactImage.style.display = "none";
+}
